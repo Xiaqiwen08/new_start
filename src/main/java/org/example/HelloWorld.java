fec6d84951abc287ca8b6b44b579177e45f5f38c
Xiaqiwen <1960649726@qq.com>
2026-06-07 12:23:55 +0800

    hot_fix commit2

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!Hello World!");
         System.out.println("Hello Git4!");
+        System.out.println("Hot_fix test!");
     }
 }
